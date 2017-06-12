11dad6573db04b9e7eafc118975decd8bb73b528
arscholtes <alex@ascholtes.me>
2017-06-12 14:56:19 -0400

    finished

--- js/main.js
+++ js/main.js
@@ -10,11 +10,10 @@ It should return the date for Indepencence Day.
 **************************************************************************************/
 ( function() {
   "use strict";
-
   //Your code here.
-
+  var date =new Date('Thu Jul 04 1776 00:00:00');
   console.log( "Independence Day date: ", date );
-  console.assert( date == "Thu Jul 04 1776 00:00:00 GMT-0400 (EDT)", "#1: Test failed. Check the parameters. Returned: " + date )
+  console.assert( date == "Thu Jul 04 1776 00:00:00", "#1: Test failed. Check the parameters. Returned: " + date )
 } )();
 
 
@@ -28,6 +27,7 @@ It should return new year's day for 2018.
   "use strict";
 
   //Your code here.
+var date =new Date('2018-1-1')
 
 console.log( "Happy new year!: ", date );
 console.assert( date == "Thu Feb 01 2018 00:00:00 GMT-0400 (EDT)", "#2: Test failed. Check the parameters. Returned: " + date )
@@ -45,9 +45,9 @@ See assertion for date and time.
   "use strict";
 
   //Your code here.
-
+var date = new Date("Mon Oct 13 1975 11:13:00");
   console.log( "Day and time: ", date );
-  console.assert( date == "Mon Oct 13 1975 11:13:00 GMT-0400 (EDT)", "#3: Test failed. Check the parameters. Returned: " + date )
+  console.assert( date == "Mon Oct 13 1975 11:13:00 GMT-0500 (EST)", "#3: Test failed. Check the parameters. Returned: " + date )
 } )();
 
 
@@ -61,8 +61,9 @@ HINT: you will need to use 'getFullYear()' for one of the variables.
   "use strict";
 
 //date variable here
+var date = new Date();
 //year variable here
-
+var year = date.getFullYear();
   console.log( "Current year: ", year );
   console.assert( year == "2017", "#4: Test failed. Check the function. Are you 'getting' the full year?. Returned: " + date )
 } )();
@@ -81,7 +82,7 @@ It should round a the `num` variable to be 184. Use the appropiate math method.
 
 //Your code here
 const num = 184.33;
-
+var roundNum = Math.floor(num);
  console.log( "roundNum: ", roundNum );
  console.assert( roundNum == "184", "#5: Test failed. Check your math method and parameters. Return: " + roundNum )
 } )();
@@ -97,7 +98,7 @@ It should return 256. Use the appropiate math method.
   "use strict";
 
   //Your code here.
-
+var power = Math.pow(-16, 2);
   console.log( "power: ", power );
   console.assert( power == "256", "#6: Test failed. Check your math method and parameters. Return: " + power )
 } )();
@@ -119,6 +120,7 @@ Edit the following object using dot notation to 'add' the missing computer compo
     processor: "i-7",
     memory: "4GB",
   };
+  computer.GPU = 'nvidia';
  //Add missing property here.
   console.log( "#10: computer", computer )
   console.assert( computer.GPU == "nvidia", "Test failed. The computer should have an 'nvidia' GPU" )
@@ -136,7 +138,12 @@ The following object is empty. Use bracket notation so that 'car' has two doors,
   var car = {};
 
 //Add properties here.
-
+car['doors']=2;
+car['wheels']=4;
+car['engine']=true;
+car['type']='sport';
+car['engine-size']="v-8";
+car['model']='mustang';
   console.log( "#11: ", car )
   console.assert( car[ "doors" ] == 2, "#11: Test failed. See required properties" )
   console.assert( car[ "wheels" ] == 4, "#11: Test failed. See required properties" )
@@ -162,8 +169,9 @@ The global variable should be 'prius'.
 
   function changeCar() {
     car = 'beattle';
+    changeCar();
+
   }
-  changeCar();
 
 
   console.log( "global variable: ", car );
@@ -184,8 +192,9 @@ function styling() {
   const language = 'CSS'
   console.log(language);
   console.assert(language == "CSS", "Test Failed. Did you compartmentalize?");
+  styling();
+
 };
-styling();
 
 console.log(language);
 console.assert(language == "HTML", "Test Failed. Did you compartmentalize?");
@@ -202,17 +211,19 @@ Where should 'multiply()' go?
 **************************************************************************************/
 
 ( function() {
-  "use strinct";
+  "use strict";
 
   function hoistThis() {
     function multiply( sum ) {
-      console.assert( sum * 2 == "8", "#9: Test failed. Check your function hoisting" );
       return sum * 2;
+      console.assert( sum * 2 == "8", "#9: Test failed. Check your function hoisting" );
+
     }
     multiply( sum );
-    console.assert( sum == "4", "#9: Test failed. Check hoisting" );
     var sum;
     sum = 2 + 2;
+    console.assert( sum == "4", "#9: Test failed. Check hoisting" );
+
   };
   hoistThis();
 } )();
